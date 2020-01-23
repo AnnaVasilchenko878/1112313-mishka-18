141cd4cd490b501cf142b81ef4a431562a1960b4
AnnaVasilchenko878 <54268958+AnnaVasilchenko878@users.noreply.github.com>
2020-01-23 19:57:25 +0300

    Исправлены ошибки

--- source/js/event.js
+++ source/js/event.js
@@ -1,19 +1,24 @@
+var buttonToggle =document.querySelector('.logo__button');
 var buttonOpen = document.querySelector('.logo__button-open');
 var buttonClose = document.querySelector('.logo__button-close');
-var siteMenuToggle = document.querySelector('.site-nav__menu');
-var buttonToggle = document.querySelector('.logo__button');
+var siteMenuToggle = document.querySelector('.main-menu');
+var siteMenuButtons = document.querySelector('.nav-controls');
 
 buttonToggle.addEventListener('click', function () {
-  if (siteMenuToggle.classList.contains('open')) {
+  if (buttonToggle.classList.contains('open')) {
 
     siteMenuToggle.classList.remove('open');
+    siteMenuButtons.classList.remove('open');
     siteMenuToggle.classList.add('close');
+    siteMenuButtons.classList.add('close');
 
     buttonOpen.classList.add('btn-hide');
     buttonClose.classList.remove('btn-hide');
   } else {
     siteMenuToggle.classList.remove('close');
+    siteMenuButtons.classList.remove('close');
     siteMenuToggle.classList.add('open');
+    siteMenuButtons.classList.add('open');
 
     buttonOpen.classList.remove('btn-hide');
     buttonClose.classList.add('btn-hide');
